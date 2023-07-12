0c8396d2aac4352712059b34c3081883ecde300a
valenoirs <luckyvw.general@gmail.com>
2023-07-13 03:00:25 +0800

    creating notes plugin

---
+++ src/api/notes/index.js
@@ -0,0 +1,12 @@
+const NotesHandler = require('./handler')
+const routes = require('./routes')
+
+module.exports = {
+  name: 'notes',
+  version: '1.0.0',
+  register: async (server, { service }) => {
+    const notesHandler = new NotesHandler()
+
+    server.route(routes(notesHandler))
+  },
+}
