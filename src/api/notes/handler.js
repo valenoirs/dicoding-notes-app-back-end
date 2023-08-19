35061f39e5805c2654ae0a103a1013955958c1e7
valenoirs <luckyvw.general@gmail.com>
2023-08-19 15:34:44 +0800

    authorization implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -17,8 +17,14 @@ class NotesHandler {
       this._validator.validateNotePayload(request.payload)
 
       const { title = 'undefined', body, tags } = request.payload
+      const { id: credentialId } = request.auth.credentials
 
-      const noteId = await this._service.addNote({ title, body, tags })
+      const noteId = await this._service.addNote({
+        title,
+        body,
+        tags,
+        owner: credentialId,
+      })
 
       return h
         .response({
@@ -48,8 +54,11 @@ class NotesHandler {
     }
   }
 
-  async getNotesHandler() {
-    const notes = await this._service.getNotes()
+  async getNotesHandler(request) {
+    const { id: credentialId } = request.auth.credentials
+
+    const notes = await this._service.getNotes(credentialId)
+
     return {
       status: 'success',
       data: { notes },
@@ -59,7 +68,11 @@ class NotesHandler {
   async getNoteByIdHandler(request, h) {
     try {
       const { id } = request.params
+      const { id: credentialId } = request.auth.credentials
+
+      await this._service.verifyNoteOwner(id, credentialId)
       const note = await this._service.getNoteById(id)
+
       return {
         status: 'success',
         data: { note },
@@ -88,8 +101,13 @@ class NotesHandler {
   async putNoteByIdHandler(request, h) {
     try {
       this._validator.validateNotePayload(request.payload)
+
       const { id } = request.params
+      const { id: credentialId } = request.auth.credentials
+
+      await this._service.verifyNoteOwner(id, credentialId)
       await this._service.editNoteById(id, request.payload)
+
       return {
         status: 'success',
         message: 'Catatan berhasil diperbarui',
@@ -118,7 +136,11 @@ class NotesHandler {
   async deleteNoteByIdHandler(request, h) {
     try {
       const { id } = request.params
+      const { id: credentialId } = request.auth.credentials
+
+      await this._service.verifyNoteOwner(id, credentialId)
       await this._service.deleteNoteById(id)
+
       return {
         status: 'success',
         message: 'Catatan berhasil dihapus',
